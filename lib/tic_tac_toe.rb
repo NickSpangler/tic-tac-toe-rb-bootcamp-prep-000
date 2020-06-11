07eea6aaa2141f60fabbae19f05a99cb05030834
Nick Spangler <spangleyman@mac.com>
2020-06-11 17:55:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,3 +51,13 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
+  end
+  turns
+end
+    
