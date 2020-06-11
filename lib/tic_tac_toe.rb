81e019519c2d4c8de445b2e4c3317a52f32c8974
Nick Spangler <spangleyman@mac.com>
2020-06-11 17:37:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,9 +10,9 @@ WIN_COMBINATIONS = [
   ]
   
   def display_board(board)
-  puts " #{board[0]} | #{moves[1]} | #{moves[2]} "
+  puts " #{board[0]} | #{board} | #{board[2]} "
   puts "-----------"
-  puts " #{moves[3]} | #{moves[4]} | #{moves[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{moves[6]} | #{moves[7]} | #{moves[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
