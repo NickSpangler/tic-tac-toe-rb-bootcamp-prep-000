81a1abe5aac6a10d9e7923bf7539dcc8ac52e3fe
Nick Spangler <spangleyman@mac.com>
2020-06-11 19:08:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -116,7 +116,7 @@ def play(board)
     if draw?(board)
       puts "Cat's Game!"
     elsif won?(board)
-      puts "Congratulations X!"
+      puts "Congratulations #{board[win_combination.first]}!"
     end
   end
 end
