0f267cfa3d3218d793e887ecbd77c19211563e17
Nick Spangler <spangleyman@mac.com>
2020-06-11 17:43:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,3 +25,11 @@ def move(board, index, token)
   board[index] = token
 end
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end 
+end
+
